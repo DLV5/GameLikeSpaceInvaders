67e30d824cf3f96ae30456583a0c4018701dd7a6
Dmitry <diman567tak@gmail.com>
2023-11-07 18:30:11 +0200

    Player moving

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,34 @@
+using System.Net;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+    [SerializeField] private InputAction _playerMoveActions;
+
+    private void OnEnable()
+    {
+        _playerMoveActions.Enable();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_playerMoveActions.IsPressed())
+        {
+            OnMove();
+        }
+    }
+
+    private void OnDisable()
+    {
+        _playerMoveActions.Disable();
+    }
+
+    private void OnMove()
+    {
+        transform.position += (Vector3) (Vector2.right * _playerMoveActions.ReadValue<float>() * _speed * Time.deltaTime);
+    }
+}
