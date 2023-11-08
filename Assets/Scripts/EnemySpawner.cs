4f804fcf92a2232b19f56e6dd2fcc35a3189a41e
Kameji <diman567tak@gmail.com>
2023-11-08 14:52:08 +0200

    Solved a problem with rectangle

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -1,12 +1,17 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
+    [Header("Spawn zone settings")]
     [SerializeField] private float _topMargin;
     [SerializeField] private float _bottomMargin;
     [SerializeField] private float _leftAndRightMargin;
+
+    [Header("Enemy amount settings")]
+    [SerializeField] private int _numberOfEnemiesInRow;
+    [SerializeField] private int _numberOfColumnsWithEnemies;
+
+    [SerializeField] private GameObject _enemyPrefab;
 
     private Vector2 _firstRectanglePoint;
     private Vector2 _secondRectanglePoint;
@@ -18,6 +23,7 @@ public class EnemySpawner : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        #region CalculatingSpawnZoneRectangle
         Vector2 _minScreenSize = Camera.main.ScreenToWorldPoint(Vector2.zero);
         Vector2 _maxScreenSize = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
 
@@ -26,10 +32,30 @@ public class EnemySpawner : MonoBehaviour
         _thirdRectanglePoint = new Vector2(_maxScreenSize.x - _leftAndRightMargin, _minScreenSize.y + _bottomMargin);
         _forthRectanglePoint = new Vector2(_minScreenSize.x + _leftAndRightMargin, _minScreenSize.y + _bottomMargin);
 
-        float _spawnZoneWidth = (_forthRectanglePoint - _thirdRectanglePoint).magnitude;
-        float _spawnZoneHeight = (_secondRectanglePoint - _firstRectanglePoint).magnitude;
+        float _spawnZoneWidth = Vector2.Distance(_firstRectanglePoint, _secondRectanglePoint);
+        float _spawnZoneHeight = Vector2.Distance(_secondRectanglePoint, _thirdRectanglePoint);
 
-        _spawnZone = new Rect(_thirdRectanglePoint, new Vector2(_spawnZoneWidth, _spawnZoneHeight));
+        Debug.LogWarning(_spawnZoneWidth);
+        Debug.LogWarning(_spawnZoneHeight);
+
+        _spawnZone = new Rect(new Vector2(0, 1.5f), new Vector2(_spawnZoneWidth, _spawnZoneHeight - 1.5f));
+
+        Debug.LogWarning(_spawnZone.width);
+        Debug.LogWarning(_spawnZone.height);
+
+        #endregion
+        //Spawning enemies
+        float sizeOfOneRow = _spawnZone.x / _numberOfEnemiesInRow;
+        float sizeOfOneColumn = _spawnZone.y / _numberOfColumnsWithEnemies;
+        for (int i = 0; i < _numberOfColumnsWithEnemies; i++)
+        {
+            float yCordinateToSpawn = sizeOfOneColumn * i + (sizeOfOneColumn - _enemyPrefab.transform.lossyScale.y - _spawnZone.y) / 2;
+            for (int j = 0; j < _numberOfEnemiesInRow; j++)
+            {
+                float xCordinateToSpawn = sizeOfOneRow * j + (sizeOfOneRow - _enemyPrefab.transform.lossyScale.x - _spawnZone.x) / 2;
+                Instantiate(_enemyPrefab, new Vector2(xCordinateToSpawn, yCordinateToSpawn), Quaternion.identity);
+            }
+        }
     }
 
     // Update is called once per frame
@@ -44,5 +70,7 @@ public class EnemySpawner : MonoBehaviour
         Gizmos.DrawLine(_secondRectanglePoint, _thirdRectanglePoint);
         Gizmos.DrawLine(_thirdRectanglePoint, _forthRectanglePoint);
         Gizmos.DrawLine(_forthRectanglePoint, _firstRectanglePoint);
+
+        Gizmos.DrawCube(_spawnZone.min, _spawnZone.max);
     }
 }
