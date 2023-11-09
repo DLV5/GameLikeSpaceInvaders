a06f19d4e7c4f11d700c1574e9735ec738f4e2f9
Kameji <diman567tak@gmail.com>
2023-11-09 09:05:29 +0200

    Created custom rectangular class

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -8,17 +8,12 @@ public class EnemySpawner : MonoBehaviour
     [SerializeField] private float _leftAndRightMargin;
 
     [Header("Enemy amount settings")]
-    [SerializeField] private int _numberOfEnemiesInRow;
-    [SerializeField] private int _numberOfColumnsWithEnemies;
+    [SerializeField] private int _numberOfEnemiesInColumn;
+    [SerializeField] private int _numberOfRowsWithEnemies;
 
     [SerializeField] private GameObject _enemyPrefab;
 
-    private Vector2 _firstRectanglePoint;
-    private Vector2 _secondRectanglePoint;
-    private Vector2 _thirdRectanglePoint;
-    private Vector2 _forthRectanglePoint;
-
-    private Rect _spawnZone;
+    private Rectangular _spawnZone;
 
     // Start is called before the first frame update
     void Start()
@@ -27,50 +22,41 @@ public class EnemySpawner : MonoBehaviour
         Vector2 _minScreenSize = Camera.main.ScreenToWorldPoint(Vector2.zero);
         Vector2 _maxScreenSize = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
 
-        _firstRectanglePoint = new Vector2(_minScreenSize.x + _leftAndRightMargin, _maxScreenSize.y - _topMargin);
-        _secondRectanglePoint = new Vector2(_maxScreenSize.x - _leftAndRightMargin, _maxScreenSize.y - _topMargin);
-        _thirdRectanglePoint = new Vector2(_maxScreenSize.x - _leftAndRightMargin, _minScreenSize.y + _bottomMargin);
-        _forthRectanglePoint = new Vector2(_minScreenSize.x + _leftAndRightMargin, _minScreenSize.y + _bottomMargin);
+        Vector2 _firstRectanglePoint = new Vector2(_minScreenSize.x + _leftAndRightMargin, _maxScreenSize.y - _topMargin);
+        Vector2 _secondRectanglePoint = new Vector2(_maxScreenSize.x - _leftAndRightMargin, _maxScreenSize.y - _topMargin);
+        Vector2 _thirdRectanglePoint = new Vector2(_maxScreenSize.x - _leftAndRightMargin, _minScreenSize.y + _bottomMargin);
+        Vector2 _forthRectanglePoint = new Vector2(_minScreenSize.x + _leftAndRightMargin, _minScreenSize.y + _bottomMargin);
 
-        float _spawnZoneWidth = Vector2.Distance(_firstRectanglePoint, _secondRectanglePoint);
-        float _spawnZoneHeight = Vector2.Distance(_secondRectanglePoint, _thirdRectanglePoint);
+        //float _spawnZoneWidth = Vector2.Distance(_firstRectanglePoint, _secondRectanglePoint);
+        //float _spawnZoneHeight = Vector2.Distance(_secondRectanglePoint, _thirdRectanglePoint);
 
-        Debug.LogWarning(_spawnZoneWidth);
-        Debug.LogWarning(_spawnZoneHeight);
-
-        _spawnZone = new Rect(new Vector2(0, 1.5f), new Vector2(_spawnZoneWidth, _spawnZoneHeight - 1.5f));
-
-        Debug.LogWarning(_spawnZone.width);
-        Debug.LogWarning(_spawnZone.height);
+        _spawnZone = new Rectangular(_firstRectanglePoint, _secondRectanglePoint, _thirdRectanglePoint, _forthRectanglePoint);
 
         #endregion
         //Spawning enemies
-        float sizeOfOneRow = _spawnZone.x / _numberOfEnemiesInRow;
-        float sizeOfOneColumn = _spawnZone.y / _numberOfColumnsWithEnemies;
-        for (int i = 0; i < _numberOfColumnsWithEnemies; i++)
+        float sizeOfOneRow = _spawnZone.Width / _numberOfEnemiesInColumn;
+        float sizeOfOneColumn = _spawnZone.Height / _numberOfRowsWithEnemies;
+        for (int i = 0; i < _numberOfRowsWithEnemies; i++)
         {
-            float yCordinateToSpawn = sizeOfOneColumn * i + (sizeOfOneColumn - _enemyPrefab.transform.lossyScale.y - _spawnZone.y) / 2;
-            for (int j = 0; j < _numberOfEnemiesInRow; j++)
+            float yCordinateToSpawn = sizeOfOneColumn * i + (sizeOfOneColumn - _enemyPrefab.transform.lossyScale.y - _spawnZone.Height) / 2;
+            for (int j = 0; j < _numberOfEnemiesInColumn; j++)
             {
-                float xCordinateToSpawn = sizeOfOneRow * j + (sizeOfOneRow - _enemyPrefab.transform.lossyScale.x - _spawnZone.x) / 2;
+                float xCordinateToSpawn = sizeOfOneRow * j + (sizeOfOneRow - _enemyPrefab.transform.lossyScale.x - _spawnZone.Width) / 2;
                 Instantiate(_enemyPrefab, new Vector2(xCordinateToSpawn, yCordinateToSpawn), Quaternion.identity);
             }
         }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     private void OnDrawGizmos()
     {
-        Gizmos.DrawLine(_firstRectanglePoint, _secondRectanglePoint);
-        Gizmos.DrawLine(_secondRectanglePoint, _thirdRectanglePoint);
-        Gizmos.DrawLine(_thirdRectanglePoint, _forthRectanglePoint);
-        Gizmos.DrawLine(_forthRectanglePoint, _firstRectanglePoint);
+        if (!Application.isPlaying)
+            return;
 
-        Gizmos.DrawCube(_spawnZone.min, _spawnZone.max);
+        Vector2[] rectangularPoints = _spawnZone.GetAllPoints();
+
+        Gizmos.DrawLine(rectangularPoints[0], rectangularPoints[1]);
+        Gizmos.DrawLine(rectangularPoints[1], rectangularPoints[2]);
+        Gizmos.DrawLine(rectangularPoints[2], rectangularPoints[3]);
+        Gizmos.DrawLine(rectangularPoints[3], rectangularPoints[0]);
     }
 }
---
+++ Assets/Scripts/Rectangular.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class Rectangular
+{
+    private Vector2 _firstRectanglePoint;
+    private Vector2 _secondRectanglePoint;
+    private Vector2 _thirdRectanglePoint;
+    private Vector2 _forthRectanglePoint;
+
+    public float Width
+    {
+        get => Vector2.Distance(_firstRectanglePoint, _secondRectanglePoint);
+    }
+
+    public float Height
+    {
+        get => Vector2.Distance(_secondRectanglePoint, _thirdRectanglePoint);
+    }
+
+    //Rectongular with points ABCD
+    public Rectangular(Vector2 point1, Vector2 point2, Vector2 point3, Vector2 point4)
+    {
+        _firstRectanglePoint = point1;
+        _secondRectanglePoint = point2;
+        _thirdRectanglePoint = point3;
+        _forthRectanglePoint = point4;
+    }
+
+    public Vector2[] GetAllPoints()
+    {
+        return new Vector2[] { _firstRectanglePoint, _secondRectanglePoint, _thirdRectanglePoint, _forthRectanglePoint };
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(_firstRectanglePoint, _secondRectanglePoint);
+        Gizmos.DrawLine(_secondRectanglePoint, _thirdRectanglePoint);
+        Gizmos.DrawLine(_thirdRectanglePoint, _forthRectanglePoint);
+        Gizmos.DrawLine(_forthRectanglePoint, _firstRectanglePoint);
+    }
+}
